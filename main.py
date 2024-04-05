b7bb26ce18edb8a6baa1e9b8433514d57510de69
niravpatel6656 <npatel8705@conestogac.on.ca>
2024-04-04 22:19:30 -0400

    Pandas Dataframe

--- main.py
+++ main.py
@@ -61,3 +61,17 @@
 #
 # for (index,row) in student_data_frames.iterrows():
 #     print(row.score)
+
+# -------------------------------------------------
+# NATO Alphabet Project
+
+import pandas
+
+# TODO 1. Create a dictionary in this format:
+nato = pandas.read_csv("nato_phonetic_alphabet.csv")
+nato_dict = {row.letter:row.code for (index,row) in pandas.DataFrame(nato).iterrows()}
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter the name").upper()
+user_output = [nato_dict[letter] for letter in user_input]
+print(user_output)
